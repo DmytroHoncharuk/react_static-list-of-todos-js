390c93070fa8a699301bc8b1addbe13d0a9fa4d4
DmytroHoncharuk <dima2005oleksandr@gmail.com>
2024-11-12 17:54:28 +0200

    update TodoInfo.jsx

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,10 +1,8 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
-import clsx from 'clsx';
 import { UserInfo } from '../UserInfo';
 
 export const TodoInfo = ({ todo }) => (
   <article
-    className={clsx('TodoInfo', todo.completed && 'TodoInfo--completed')}
+    className={`TodoInfo ${todo.completed ? 'TodoInfo--completed' : ''}`}
   >
     <h2 className="TodoInfo__title">{todo.title}</h2>
     <UserInfo user={todo.user} />
